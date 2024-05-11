067830dc28d2664ac63b215496227eeca0afb8fb
Jakub Havej <jakub.havej@momence.com>
2024-05-11 16:13:37 +0200

    gitignore update

--- packages/db/dist/eslint.config.d.ts
+++
@@ -1,3 +0,0 @@
-declare const _default: import('typescript-eslint').Config;
-export default _default;
-//# sourceMappingURL=eslint.config.d.ts.map
--- packages/db/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-declare global {
-    var prisma: PrismaClient | undefined;
-}
-export declare const prisma: PrismaClient<import(".prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
-export * from "@prisma/client";
-//# sourceMappingURL=index.d.ts.map
