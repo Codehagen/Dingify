f9c8e920fe9e1bd8241d41a0daeaede4c86e1946
Amirthananth <amirthanand@gmail.com>
2024-05-14 23:26:51 +0530

    feat: Copy API Key Component (#25)
    fixes #18

--- apps/www/src/components/providers.tsx
+++ apps/www/src/components/providers.tsx
@@ -3,11 +3,16 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 import { Provider as BalancerProvider } from "react-wrap-balancer";
+import { TooltipProvider } from "@dingify/ui/components/tooltip"
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
-      <BalancerProvider>{children}</BalancerProvider>
+      <BalancerProvider>
+        <TooltipProvider delayDuration={0}>
+            {children}
+        </TooltipProvider>
+      </BalancerProvider>
     </NextThemesProvider>
   );
 }
