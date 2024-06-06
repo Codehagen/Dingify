7e6eef0b944b89560f71911989dcf3799c334dc2
Christer <Christer.hagen@gmail.com>
2024-06-06 08:06:04 +0200

    55 feature update landingpage (#56)
    * first update on landingpage
    
    * update sphere bottom
    
    * sphere v2 done
    
    * before hv
    
    * new button

--- apps/www/src/app/(marketing)/layout.tsx
+++ apps/www/src/app/(marketing)/layout.tsx
@@ -1,8 +1,9 @@
 import { Suspense } from "react";
-import { NavBar } from "@/components/layout/navbar";
-import { SiteFooter } from "@/components/layout/site-footer";
+
 import { marketingConfig } from "@/config/marketing";
 import { getCurrentUser } from "@/lib/session";
+import { NavBar } from "@/components/layout/navbar";
+import { SiteFooter } from "@/components/layout/site-footer";
 
 interface MarketingLayoutProps {
   children: React.ReactNode;
@@ -14,11 +15,11 @@ export default async function MarketingLayout({
   const user = await getCurrentUser();
 
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="">
       <Suspense fallback="...">
         <NavBar user={user} items={marketingConfig.mainNav} scroll={true} />
       </Suspense>
-      <main className="flex-1">{children}</main>
+      <main className="mx-auto flex-1 overflow-hidden">{children}</main>
       <SiteFooter />
     </div>
   );
