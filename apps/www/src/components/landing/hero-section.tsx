39ce2a4b272187d1583210f0e1ac5548cb2c45d6
Christer <Christer.hagen@gmail.com>
2024-06-16 21:25:09 +0200

    first commit onboarda (#69)
    * first commit onboarda
    
    * update packages
    
    * onborda setup (#71)
    
    * onborda setup
    
    * update lock
    
    
    Co-authored-by: Matt <mattsince87@gmail.com>

--- apps/www/src/components/landing/hero-section.tsx
+++ apps/www/src/components/landing/hero-section.tsx
@@ -18,7 +18,10 @@ export default function HeroSectionNew2() {
       id="hero"
       className="relative mx-auto mt-32 max-w-[80rem] px-6 text-center md:px-8"
     >
-      <div className="backdrop-filter-[12px] group inline-flex h-7 translate-y-[-1rem] animate-fade-in items-center justify-between gap-1 rounded-full border border-white/5 bg-white/10 px-3 text-xs text-white opacity-0 transition-all ease-in hover:cursor-pointer hover:bg-white/20 dark:text-black">
+      <div
+        id="onborda-step1"
+        className="backdrop-filter-[12px] group inline-flex h-7 translate-y-[-1rem] animate-fade-in items-center justify-between gap-1 rounded-full border border-white/5 bg-white/10 px-3 text-xs text-white opacity-0 transition-all ease-in hover:cursor-pointer hover:bg-white/20 dark:text-black"
+      >
         <TextShimmer className="inline-flex items-center justify-center">
           <span>✨ Introducing Dingify</span>{" "}
           <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
